46c1d1fc60efcc8db5a6ac747c333e1fa2ba8921
Acedo, Mark Relan Gercee <125295018+mawrk19@users.noreply.github.com>
2025-05-06 21:39:03 +0800

    added icons

--- alalai/src/app/components/navBar.tsx
+++ alalai/src/app/components/navBar.tsx
@@ -5,13 +5,13 @@ import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import Button from '@mui/material/Button';
 import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import AutoModeIcon from '@mui/icons-material/AutoMode';
+import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+import InfoIcon from '@mui/icons-material/Info';
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = React.useState(false);
@@ -22,25 +22,13 @@ export default function TemporaryDrawer() {
 
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+
       <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+        {['New Chat', 'Model', 'About'].map((text, index) => (
           <ListItem key={text} disablePadding>
             <ListItemButton>
               <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                {index === 0 ? <PlaylistAddIcon/> : index === 1 ? <AutoModeIcon/> : <InfoIcon/> }
               </ListItemIcon>
               <ListItemText primary={text} />
             </ListItemButton>
